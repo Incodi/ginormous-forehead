e1c1a81740d672c16389a33095458be889cbe0b6
Incodi <frogeaterfromdoomland@gmail.com>
2024-12-16 22:51:45 -0500

    Added varied strings

--- crain.js
+++ crain.js
@@ -5,6 +5,30 @@
 */
 
 document.addEventListener('scroll', function () {
-    var pre = document.querySelector( 'pre' );
-    pre.innerText = pre.innerText + '            |                   |  \n'
-  });
+  var pre = document.querySelector( 'pre' );
+  var my_string = '            |                   |  \n' 
+  var my_string2 = '            |watch a crainer vid|eo or else... \n' 
+  var my_string3 = '            |subscribe to craine|r \n' 
+  var my_string4 = '            |slogo is cute      | \n' 
+  var my_string5 = '            |crainer is cool    | \n' 
+  var my_string6 = '            |you are so lucky if| you see this \n' 
+  var my_stringOMG = '          |Actually INSANE tha|t you got this message \n' 
+
+  if (Math.random() < 0.008) { 
+    pre.innerText += my_string2;
+  }
+  if (Math.random() < 0.008) {
+    pre.innerText += my_string3;
+  }
+  if (Math.random() < 0.001) {
+    pre.innerText += my_string5;
+  }
+  if (Math.random() < 0.01) {
+    pre.innerText += my_string6;
+  }
+  if (Math.random() < 0.0001) {
+    pre.innerText += my_string7;
+  } 
+
+  pre.innerText += my_string.repeat(10)
+});
