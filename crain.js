f8363082d868f82048bf42360db3f307253f7085
Incodi <frogeaterfromdoomland@gmail.com>
2024-12-17 14:39:35 -0500

    Added animal cases

--- crain.js
+++ crain.js
@@ -7,19 +7,79 @@
 document.addEventListener('scroll', function () {
   var pre = document.querySelector( 'pre' );
   var my_string = '            |                   |  \n';
-  var my_string2 = '            |watch a crainer vid|eo or else... \n'; 
+  var orElse = '            |watch a crainer vid|eo or else... \n'; 
   var my_string3 = '            |subscribe to craine|r \n';
   var my_string4 = '            |slogo is cool      | \n';
   var my_string5 = '            |crainer is cool    | \n';
   var my_string6 = '            |you are so lucky if| you see this \n';
+  var monkey = 
+  `            |                   |  
+            |                   |    __
+            |                   w  c(..)o   (
+            |                   |\\__(-)    __)
+            |                   |    /\\   (
+            |                   |   /(_)___)
+            |                   |   w /|
+            |                   |    | \\
+            |                   |    m  m
+`;
 
-  // Rare cases for extreme scrollers
+var butterfly =
+`(\\o/)       |                   |
+(/|\\)       |                   |
+`;
+
+var birb =
+`            |                   |  
+            |                   |   .-.
+            |                   |  /'v'\\
+            |                   | (/   \\)
+            |                   |='="="===< 
+            |                   |   |_|
+`;
+var spider =
+`            |                   |  
+            |                   |
+            |       / _ \\       |    
+            |     \\_\\(_)/_/     |
+            |      _//"\\\\_      |
+            |       /   \\       |
+`;
+var camel =
+`            |                   |  
+            |                   |     //
+            |                   |   _oo\\
+            |                   |  (__/ \\  _  _
+            |                   |     \\  \\/ \\/ \\
+            |                   |     (         )\\
+            |                   |      \\_______/  \\
+            |                   |       [[] [[]
+            |                   |       [[] [[]
+`;
+
+  // Rare cases for extreme scrollers, collected with addThis
 
   var addThis = '';
 
-  if (Math.random() < 0.007) { 
-    addThis += my_string2;
+  // Image cases
+  if (Math.random() < 0.001) { 
+    addThis += monkey;
   }
+  if (Math.random() < 0.001) { 
+    addThis += butterfly;
+  }
+  if (Math.random() < 0.001) { 
+    addThis += birb;
+  }
+  if (Math.random() < 0.0005) { 
+    addThis += spider;
+  }
+  if (Math.random() < 0.001) { 
+    addThis += camel;
+  }
+
+
+ // Text cases
   if (Math.random() < 0.008) {
     addThis += my_string3;
   }
@@ -32,8 +92,11 @@ document.addEventListener('scroll', function () {
   if (Math.random() < 0.0001) {
     addThis += my_string6;
   } 
+  if (Math.random() < 0.007) { 
+    addThis += orElse;
+  }
 
-  addThis += my_string.repeat(10);
+  addThis += my_string.repeat(15);
   pre.innerText += addThis;
 
 });
